4d6c2fdd099452ec2024959cd1052c7df1fa2520
yanchristofers <yanchristofers@gmail.com>
2023-05-24 20:00:39 +0700

    habis delet yang sebelumnya

--- r_listpertandingan/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RListpertandinganConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'r_listpertandingan'
--- r_listpertandingan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from r_listpertandingan.views import show_listpertandingan
-from . import views
-
-app_name = 'r_listpertandingan'
-
-urlpatterns = [
-    path('listpertandingan/', views.show_listpertandingan, name='show_listpertandingan'),
-]
